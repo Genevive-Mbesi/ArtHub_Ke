57ff328bb1c45ebfd7763a1d5fd7e27ae3aba1b9
Genevive-Mbesi <genmbesi@gmail.com>
2024-01-18 11:08:03 +0300

    Navigation

--- mbesiart/src/components/home.jsx
+++ mbesiart/src/components/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import assets from '../assets/cover.png';
 
-export default function Home() {
+const Home = () => {
   const bodyStyle = {
     backgroundImage: `url(${assets})`,
     backgroundRepeat: 'no-repeat',
@@ -12,14 +12,32 @@ export default function Home() {
     height: '100vh',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white', // Text color, adjust as needed
+    justifyContent: 'flex-start',
+    color: 'brown', // Text color, adjust as needed
+     // Center-align the text
     // Add any other styles you need for your content
   };
 
+  const headingStyle = {
+    textDecoration: 'underline',
+    textDecorationColor: 'brown',
+    padding: '20px', 
+    fontWeight: 'bold' 
+  };
+
+  
+  
+
   return (
     <body style={bodyStyle}>
-      {/* Your content goes here */}
+       <div style={{ width: '80%', maxWidth: '600px',marginLeft: '50px' }}>
+        <h1 style={headingStyle}>Profile</h1>
+        <h1 style={headingStyle}>Services</h1>
+        <h1 style={headingStyle}>Listings</h1>
+        <h1 style={headingStyle}>Contact</h1>
+      </div>
     </body>
   );
-}
+};
+
+export default Home;
