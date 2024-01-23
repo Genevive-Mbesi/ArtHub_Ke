69c527ef96f1d7ca52798485d1c359fb2f468e67
Genevive-Mbesi <genmbesi@gmail.com>
2024-01-23 15:50:36 +0300

    listings

--- mbesiart/src/components/listings.jsx
+++ mbesiart/src/components/listings.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
 
-const Listings = () => {
-  <div></div>
+const Listings = React.forwardRef ((props,ref) => { 
+  const bodyStyle = {
+    backgroundColor: 'Gainsboro',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    margin: 0,
+    padding: 0,
+    height: '100vh',
+    alignItems: 'center',
+  };
+  const subheading = {
+    fontWeight: '300', 
+    fontSize: '2em',
+    color:'DarkSlateGrey', 
+    paddingTop: '10px',
+    position: 'absolute', 
+    top: '30%',
+    left: '50%',
+   transform: 'translateX(-50%)',
+   fontFamily: '  Georgia,serif',
+  };
+  const myName = {
+    fontWeight: '300', 
+    fontSize: 'em',
+    color:'DarkSlateGrey', 
+    position: 'absolute', 
+    top: '57%',
+    left: '50%',
+   transform: 'translateX(-50%)',
+   fontFamily: '  Georgia,serif',
+   borderBottom:'2px solid DarkSlateGrey',
+  };
+  
+  return(
+    <div ref={ref} >
+    <div style={bodyStyle}>
 
-};
+    <h1 style={subheading}> Art is a friendly of expressing our imagination,
+     It speaks a lot about our world and our lives.</h1>
+     <h1 style={myName}> Genevive Mbesi, CEO</h1>
+    </div>
+    </div> 
+  
+  );
+});
 
 export default Listings;
