e682e1052dee1c055fd154a0cd4c57d8e3068184
Genevive-Mbesi <genmbesi@gmail.com>
2024-01-26 14:27:17 +0300

    profile

--- mbesiart/src/components/profile.jsx
+++ mbesiart/src/components/profile.jsx
@@ -9,7 +9,7 @@ const Profile = React.forwardRef((props, ref) => {
     backgroundSize: 'cover',
     margin: 0,
     padding: 0,
-    height: '100vh',
+    minHeight: '100vh', // Using minHeight to ensure the content is centered vertically
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -17,13 +17,10 @@ const Profile = React.forwardRef((props, ref) => {
   };
 
   const profileimage = {
-    position: 'absolute',
-    top: '30%',
-    left: '50%',
-    marginLeft: '100px',
     width: '400px', // Set the desired width
     height: 'auto',
     objectFit: 'cover',
+    marginBottom: '20px', // Added margin for spacing
   };
 
   const content = {
@@ -32,10 +29,7 @@ const Profile = React.forwardRef((props, ref) => {
     fontSize: '2em',
     color: 'DarkSlateGrey',
     fontFamily: 'Georgia, serif',
-    marginLeft: '50px',
-    left:'5%',
-    position: 'absolute',
-    top: '30%',
+    textAlign: 'center', // Center text for responsiveness
   };
 
   const subcontent = {
@@ -45,34 +39,15 @@ const Profile = React.forwardRef((props, ref) => {
     fontSize: '1em',
     color: 'DarkSlateGrey',
     fontFamily: 'Georgia, serif',
-    marginLeft: '50px',
-    position: 'absolute',
-    top: '45%',
-    left:'5%',
-  };
-
-  // Media query for smaller screens
-  const mediaQueryStyle = {
-    '@media (max-width: 768px)': {
-      profileimage: {
-        width: '100%',
-        marginLeft: '0',
-      },
-      content: {
-        marginLeft: '0',
-      },
-      subcontent: {
-        marginLeft: '0',
-      },
-    },
+    textAlign: 'center', // Center text for responsiveness
   };
 
   return (
     <div ref={ref}>
       <div style={bodyStyle}>
         <img src={assets} alt="profile" className='w-80 h-30 overflow-hidden container' style={profileimage} />
-        <h1 style={{ ...content, ...mediaQueryStyle.content }}>WALLS THAT MAKE <br /> YOU WANT TO STAY</h1>
-        <h2 style={{ ...subcontent, ...mediaQueryStyle.subcontent }}>Our specially designed Wall Art is created to<br />
+        <h1 style={content}>WALLS THAT MAKE <br /> YOU WANT TO STAY</h1>
+        <h2 style={subcontent}>Our specially designed Wall Art is created to<br />
           especially give you an in-tune feeling with your environment<br />
           whether at home, office, leisure time, or hosting special events,<br />
           think of this as an elevator to your mood every time<br />
