4f7bffc5fedd6241ece30c927aad86d9f6ee2dde
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-05 18:57:04 +0300

    nav

--- mbesiart/src/components/navigationbar.jsx
+++ mbesiart/src/components/navigationbar.jsx
@@ -1,44 +1,8 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { AiOutlineMenu } from "react-icons/ai";
 
-const Navigation = () => {
-
-  const headingStyle = {
-    padding: '10px',
-    paddingBottom: '10px',
-    marginBottom: '10px',
-    color: 'DarkSlateGrey',
-    cursor: 'pointer',
-    fontFamily: 'Georgia, serif',
-  };
-
-  const itemStyle = {
-    padding: '0.2rem',
-    marginBottom: '0.2rem',
-    color: 'DarkSlateGrey',
-    cursor: 'pointer',
-    fontFamily: 'Georgia, serif',
-    margin: '2px',
-  };
-
-  const containerStyle = {
-    display: 'flex',
-    padding: '0.2rem',
-    borderRadius: '10px',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '30px',
-    margin: '5px',
-  };
-  
-  const navStyle = {
-    width: '100%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    textAlign: 'center', // Center the content
-  };
-
+export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -46,56 +10,42 @@ const Navigation = () => {
   };
 
   return (
-    <nav style={navStyle}>
-      <h1 onClick={toggleMenu} className="cursor-pointer">
-        <AiOutlineMenu />
-      </h1>
-      <ul
-        className={`${
-          showMenu
-            ? "flex flex-col bg-teal-800 p-4 rounded-lg"
-            : "hidden"
-        } lg:flex lg:flex-row lg:items-center mt-2`}
-        style={containerStyle}
-      >
-        {/* Render only on large screens */}
-        <li style={itemStyle}>
-          <Link to="/profile" style={headingStyle}>
-            Profile
-          </Link>
-        </li>
-        <li style={itemStyle}>
-          <Link to="/services" style={headingStyle}>
-            Services
-          </Link>
-        </li>
-        <li style={itemStyle}>
-          <Link to="/listings" style={headingStyle}>
-            Team
-          </Link>
-        </li>
-        <li style={itemStyle}>
-          <Link to="/contacts" style={headingStyle}>
-            Contacts
-          </Link>
-        </li>
-        <li style={itemStyle}>
-          <a href="https://www.instagram.com/wall_art_heart?igsh=OHk4bmZrb3RkcjVy" style={headingStyle}>
-            Designs
-          </a>
-        </li>
-      </ul>
-
-      {/* Render only on small screens */}
-      <div className={`lg:hidden ${showMenu ? "block" : "hidden"}`}>
-        <ul style={containerStyle}>
-          <li style={itemStyle}>
-            <AiOutlineMenu />
+    <div className="container items-center justify-center mx-auto text-gray-100 text-s">
+      <nav className="py-5 pl-4 lg:pl-0 flex flex-col lg:flex-row lg:justify-between fixed top-0 left-0 right-0  z-10">
+        <div className="flex items-center">
+          <div className="lg:hidden lg:flex flex-col lg:items-center absolute top-8 right-4 mt-2">
+            <h1 onClick={toggleMenu} className="cursor-pointer">
+              <AiOutlineMenu />
+            </h1>
+          </div>
+        </div>
+        <ul
+          className={`${
+            showMenu
+              ? "flex flex-col bg-teal-800 p-4 rounded-lg"
+              : "hidden"
+          } lg:flex lg:flex-row lg:items-center absolute top-12 right-4 text-s flex mt-2`}
+        >
+          <li className=" mb-4 lg:mr-10  ">
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li className=" mb-4 lg:mr-10">
+            <Link to="/services">Services</Link>
+          </li>
+          <li className=" mb-4 lg:mr-10">
+            <Link to="/listings">listings</Link>
+          </li>
+          
+          <li className=" mb-4 lg:mr-10">
+            <Link to="/contacts">contacts</Link>
+          </li>
+          <li className=" mb-4 " >
+            <a href="https://www.instagram.com/wall_art_heart?igsh=OHk4bmZrb3RkcjVy">
+              Designs
+            </a>
           </li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
-};
-
-export default Navigation;
+}
