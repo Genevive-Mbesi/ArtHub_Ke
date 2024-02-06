52c1213faf03b14c34a179a4c52f46244c3c5b10
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-06 14:40:07 +0300

    layout adjustment

--- mbesiart/src/components/navigationbar.jsx
+++ mbesiart/src/components/navigationbar.jsx
@@ -1,7 +1,6 @@
-import React ,{ useState }from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AiOutlineMenu } from "react-icons/ai";
-
+import { AiOutlineMenu } from 'react-icons/ai';
 
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -25,8 +24,7 @@ const Navigation = () => {
     color: 'DarkSlateGrey',
     cursor: 'pointer',
     fontFamily: 'Georgia, serif',
-    margin:'2px',
-    
+    margin: '2px',
   };
 
   const containerStyle = {
@@ -38,31 +36,27 @@ const Navigation = () => {
     paddingTop: '30px',
     margin: '5px',
   };
+
   const navStyle = {
     width: '100%',
     marginLeft: 'auto',
     marginRight: 'auto',
   };
-  
 
   return (
     <nav style={navStyle}>
       <div className="lg:hidden lg:flex flex-col lg:items-center absolute top-8 right-4 mt-2">
-  
-  <h1 onClick={toggleMenu} className="cursor-pointer">
-    <AiOutlineMenu />
-  </h1>
-</div>
-      <ul 
-      className={`${
-        showMenu
-          ? "flex flex-col bg-grey-800 p-4 rounded-lg"
-          : "hidden"
-      } lg:flex lg:flex-row lg:items-center `}
-    
-    
-      style={containerStyle}>
-        <li className='mb-4 lg:mr-10'style={itemStyle}>
+        <h1 onClick={toggleMenu} className="cursor-pointer">
+          <AiOutlineMenu />
+        </h1>
+      </div>
+      <ul
+        className={`${
+          showMenu ? 'flex flex-col bg-grey-800 p-4 rounded-lg' : 'hidden'
+        } lg:flex lg:flex-row lg:items-center `}
+        style={containerStyle}
+      >
+        <li className="mb-4 lg:mr-10" style={itemStyle}>
           <Link to="/profile" style={headingStyle}>
             Profile
           </Link>
@@ -83,7 +77,10 @@ const Navigation = () => {
           </Link>
         </li>
         <li style={itemStyle}>
-          <a href="https://www.instagram.com/wall_art_heart?igsh=OHk4bmZrb3RkcjVy" style={headingStyle}>
+          <a
+            href="https://www.instagram.com/wall_art_heart?igsh=OHk4bmZrb3RkcjVy"
+            style={headingStyle}
+          >
             Designs
           </a>
         </li>
