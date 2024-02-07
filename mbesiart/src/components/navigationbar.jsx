dd6e28923d2feb59c32f39ca07289fb73af449d2
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-07 03:05:40 +0300

    layout adjustment

--- mbesiart/src/components/navigationbar.jsx
+++ mbesiart/src/components/navigationbar.jsx
@@ -34,7 +34,7 @@ export default function Navbar() {
         <ul
           className={`${
             showMenu
-              ? "flex flex-col  font-serif lg:hidden p-4 rounded-lg"
+              ? "flex flex-col  font-serif bg-gray-100 lg:hidden p-4 rounded-lg"
               : "hidden"
           } lg:flex lg:flex-row lg:items-center absolute top-12 right-4 text-s flex mt-2`}
         >
