a33b5a6c930a4badb5dc69c7d5dcf26e60c2c4fd
Genevive-Mbesi <genmbesi@gmail.com>
2024-02-07 14:09:02 +0300

    home page

--- mbesiart/src/components/navigationbar.jsx
+++ mbesiart/src/components/navigationbar.jsx
@@ -8,24 +8,29 @@ export default function Navbar() {
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
-  const itemStyle = {
-    padding: '0.2rem',
-    marginBottom: '0.2rem',
-    color: 'DarkSlateGrey',
+ 
+  const navContainer = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  };
+  const headingStyle = {
+    backgroundColor: 'DarkSlateGrey',
+    color: 'Gainsboro',
+    padding: '30px',
+    fontFamily: '  Georgia,serif',
+    color: 'white',
     cursor: 'pointer',
-    borderRadius: '10px',
     fontFamily: 'Georgia, serif',
-    margin:'2px',
-    border: '1px solid DarkSlateGrey',
-    
-    
+    alignItems:'center',
+    justifyContent:'center',
   };
   
   return (
-    <div className="container items-center justify-center font-serif text-teal-800 text-s">
-      <nav className="">
+    <div style={headingStyle} >
+      <nav style={navContainer} >
         <div className="flex items-center">
-          <div className="lg:hidden lg:flex flex-col lg:items-center absolute top-4 right-4">
+          <div className="lg:hidden lg:flex flex-col lg:items-center">
             <h1 onClick={toggleMenu} className="cursor-pointer">
               <AiOutlineMenu />
             </h1>
@@ -34,31 +39,31 @@ export default function Navbar() {
         <ul
           className={`${
             showMenu
-              ? "flex flex-col  font-serif bg-gray-100 lg:hidden p-4 rounded-lg"
+              ? "flex flex-col  font-serif bg-gray-100 lg:hidden rounded-lg"
               : "hidden"
-          } lg:flex lg:flex-row lg:items-center absolute top-12 right-4 text-s flex mt-2`}
+          } lg:flex lg:flex-row lg:items-center flex`}
         >
-          <li style={itemStyle}>
+          <li style={headingStyle}>
             <Link to="/profile" >
               Profile
             </Link>
           </li>
-          <li style={itemStyle}>
+          <li style={headingStyle}>
             <Link to="/services">
               Services
             </Link>
           </li>
-          <li style={itemStyle}>
+          <li style={headingStyle}>
             <Link to="/listings">
               Listings
             </Link>
           </li>
-          <li style={itemStyle}>
+          <li style={headingStyle}>
             <Link to="/contacts">
               Contacts
             </Link>
           </li>
-          <li style={itemStyle}>
+          <li style={headingStyle}>
             <a
               href="https://www.instagram.com/wall_art_heart?igsh=OHk4bmZrb3RkcjVy">
               Designs
